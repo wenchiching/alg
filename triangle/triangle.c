903015771282cdde24ce091d180fe97fb3e30ddd
owen_wen <owen_wen@htc.com>
2013-08-10 13:46:52 +0800

    triangle phase2, FC

--- triangle/triangle.c
+++ triangle/triangle.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 
 void swap(int* a, int* b){
-    int* c = NULL;
-    c = a;
-    a = b;
-    b = c;
+    int c = 0;
+    c = *a;
+    *a = *b;
+    *b = c;
 }
 
 int main(){
     int a = 0, b = 0, c = 0;
     scanf("%d %d %d", &a, &b, &c);
-    if( a<b ) swap(&a,&b);
-    printf("%d %d", a, b);
+    if( a < b ) swap(&a,&b);
+    if( a < c ) swap(&a,&c);
+    if( b < c ) swap(&b,&c);
+    if( a >= b+c ) printf("not a triangle");
+    if( a*a == b*b+c*c ){
+        printf("yes");
+    }
+    else{
+        printf("no");
+    }
     return 0;
 }
