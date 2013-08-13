22ee735aa7d947afded36c69d6a7c0447268617d
owen_wen <owen_wen@htc.com>
2013-08-13 20:59:21 +0800

    hanxin phase2

--- chapter2/hanxin/hanxin.c
+++ chapter2/hanxin/hanxin.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
 int main(){
-    int i = 0;
+    int i = 0, a = 0, b = 0, c = 0;
+    FILE *fin, *fout;
+    fin = fopen("hanxin.in", "r");
+    fout = fopen("hanxin.out", "w");
+    fscanf(fin, "%d %d %d", &a, &b, &c);
     for( i = 10 ; i <= 100 ; i++){
-
+        
     }
     return 0;
 }
