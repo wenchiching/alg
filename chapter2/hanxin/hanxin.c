f51894b450168625fc2f7608dc315cecefe53ba5
owen_wen <owen_wen@htc.com>
2013-08-17 14:38:01 +0800

    hanxin phase3, FC

--- chapter2/hanxin/hanxin.c
+++ chapter2/hanxin/hanxin.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
 int main(){
-    int i = 0, a = 0, b = 0, c = 0;
+    int i = 0, a = 0, b = 0, c = 0, ans = 0;
     FILE *fin, *fout;
     fin = fopen("hanxin.in", "r");
     fout = fopen("hanxin.out", "w");
     fscanf(fin, "%d %d %d", &a, &b, &c);
     for( i = 10 ; i <= 100 ; i++){
-        
+        if ( i%3 == a && i%5 == b && i%7 == c ){
+            fprintf(fout, "%d", i);
+            ans = 1;
+            break;
+        }
+    }
+    if ( ans == 0 ){
+        fprintf(fout, "No answer");
     }
     return 0;
 }
