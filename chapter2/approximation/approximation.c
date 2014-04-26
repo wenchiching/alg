6b29faad71a752c2dc202cb4d19bcbed6651f3ca
owen_wen <owen_wen@htc.com>
2014-04-26 22:57:38 +0800

    approximation

--- chapter2/approximation/approximation.c
+++ chapter2/approximation/approximation.c
@@ -3,6 +3,17 @@
 #include <math.h>
 
 int main(){
-	printf("%f\n", M_PI);
+	float i = 1, j = 0, pi = 0;
+	int sign = -1;
+	while(1){
+		j = 1.0/(2*i-1);
+		pi += (j)*sign;
+		i++;
+		sign = -sign;
+		printf("%f\n", pi*4);
+		if(j<0.000001){
+			break;
+		}
+	}
 	return 0;
 }
