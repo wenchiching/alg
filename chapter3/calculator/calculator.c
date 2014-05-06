1dffb5bd7460e9e210e272b9c335294f36ee3183
owen_wen <owen_wen@htc.com>
2014-05-07 00:08:22 +0800

    calculator

--- chapter3/calculator/calculator.c
+++ chapter3/calculator/calculator.c
@@ -3,11 +3,23 @@
 int main(){
 	FILE *f1 = NULL;
 	int num1 = 0, num2 = 0;
-	char char1[10] = {0};
+	char buf[100], op='\0';
 	f1 = fopen("calculator.in", "r");
-	do{
-		fscanf(f1, "%d%*s%d", &num1, char1, &num2);
-		printf("%d%s%d\n", num1, char1, num2);
-	}while( 1 );
+	while( fgets(buf, sizeof(buf), f1) != NULL ){
+		sscanf(buf, "%d %c %d", &num1, &op, &num2);
+		printf("%d %c %d\n", num1, op, num2);
+		if( op == '+' ){
+			// +
+			printf("%d\n", num1+num2);
+		}
+		else if( op == '*' ){
+			// *
+			printf("%d\n", num1*num2);
+		}
+		else if( op == '-' ){
+			// -
+			printf("%d\n", num1-num2);
+		}
+	}
 	return 0;
 }
